473bc51541584e3de4b8c4bcd5b936f922653f70
dude1998devIndian <dude1998dev@gmail.com>
2026-03-13 16:27:22 +0530

    resume download

--- src/app/features/hero/hero.ts
+++ src/app/features/hero/hero.ts
@@ -1,5 +1,5 @@
-﻿import { Component, Input } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+import { Component, Inject, Input } from '@angular/core';
+import { DOCUMENT, NgFor, NgIf } from '@angular/common';
 import { Resume } from '../../core/models/resume.model';
 import { NavbarComponent } from '../../shared/components/navbar/navbar';
 
@@ -12,4 +12,12 @@ import { NavbarComponent } from '../../shared/components/navbar/navbar';
 })
 export class HeroComponent {
   @Input() resume!: Resume;
+
+  downloadResumeUrl: string;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    const baseHref = this.document.querySelector('base')?.getAttribute('href') ?? '/';
+    const normalizedBaseHref = baseHref.endsWith('/') ? baseHref : `${baseHref}/`;
+    this.downloadResumeUrl = `${normalizedBaseHref}assets/RESUME_Hemanth_Kumar_p.pdf`;
+  }
 }
